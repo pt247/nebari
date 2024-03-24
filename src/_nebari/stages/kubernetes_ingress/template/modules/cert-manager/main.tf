a0e9497b51a6788c384ccb27fb08d1f41248a909
Prashant Tiwari <prashanttiwari247@gmail.com>
2024-03-24 14:25:34 +0000

    Switch to YAML for testing.

--- src/_nebari/stages/kubernetes_ingress/template/modules/cert-manager/main.tf
+++ src/_nebari/stages/kubernetes_ingress/template/modules/cert-manager/main.tf
@@ -15,108 +15,53 @@ resource "helm_release" "cert_manager" {
 
 }
 
-## TODO: Pick staging on non-dev evns
-#resource "kubernetes_manifest" "clusterissuer_letsencrypt_staging" {
-#  manifest = {
-#    "apiVersion" = "cert-manager.io/v1"
-#    "kind" = "ClusterIssuer"
-#    "metadata" = {
-#      "name" = "letsencrypt-staging"
-#    }
-#    "spec" = {
-#      "acme" = {
-#        "email" = "var.cluster_issuer_email"
-#        "privateKeySecretRef" = {
-#          "name" = "letsencrypt-staging"
-#        }
-#        "server" = "https://acme-staging-v02.api.letsencrypt.org/directory"
-#        "solvers" = [
-#          {
-#            "http01" = {
-#              "ingress" = {
-#                "ingressClassName" = "traefik"
-#              }
-#            }
-#          },
-#        ]
-#      }
-#    }
-#  }
-#}
 
-
-resource "kubernetes_manifest" "clusterissuer_letsencrypt_production" {
-  manifest = {
-    "apiVersion" = "cert-manager.io/v1"
-    "kind" = "ClusterIssuer"
-    "metadata" = {
-      "name" = "letsencrypt-production"
-    }
-    "spec" = {
-      "acme" = {
-        "email" = var.acme-email
-        "privateKeySecretRef" = {
-          "name" = "letsencrypt-production"
-        }
-        "server" = var.acme-server
-#        "solvers" = [
-#          {
-#            "http01" = {
-#              "ingress" = {
-#                "ingressClassName" = "traefik"
-#              }
-#            }
-#          },
-#        ]
-      }
-    }
-  }
+resource "kubernetes_manifest" "clusterissuer_letsencrypt_staging" {
+  manifest = <<YAML
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+ name: letsencrypt-staging
+ namespace: dev
+spec:
+ acme:
+   email: ptiwari@quansight.com
+   server: https://acme-staging-v02.api.letsencrypt.org/directory
+   privateKeySecretRef:
+     name: letsencrypt-staging
+   solvers:
+     - http01:
+         # The ingressClass used to create the necessary ingress routes
+         ingress:
+           class: traefik
+YAML
 }
 
 
 
-#resource "kubernetes_manifest" "certificate_local_nebari_dev" {
-#  manifest = {
-#    "apiVersion" = "cert-manager.io/v1"
-#    "kind" = "Certificate"
-#    "metadata" = {
-#      "name" = "local-nebari-dev"
-#      "namespace" = "default"
-#    }
-#    "spec" = {
-#      "commonName" = "*.local-nebari-dev"
-#      "dnsNames" = [
-#        "local-nebari-dev",
-#        "*.local-nebari-dev",
-#      ]
-#      "issuerRef" = {
-#        "kind" = "ClusterIssuer"
-#        "name" = "letsencrypt-staging"
-#      }
-#      "secretName" = "local-nebari-dev-staging-tls"
-#    }
-#  }
-#}
 
 resource "kubernetes_manifest" "certificate_local_nebari_dev" {
-  manifest = {
-    "apiVersion" = "cert-manager.io/v1"
-    "kind" = "Certificate"
-    "metadata" = {
-      "name" = "local-nebari.dev"
-      "namespace" = var.namespace_name
-    }
-    "spec" = {
-      "commonName" = "*.local-nebari.dev"
-      "dnsNames" = [
-        "local-nebari.dev",
-        "*.local-nebari.dev",
-      ]
-      "issuerRef" = {
-        "kind" = "ClusterIssuer"
-        "name" = "letsencrypt-production"
-      }
-      "secretName" = "local-nebari-dev-tls"
-    }
-  }
+  manifest = <<YAML
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: local-nebari.dev
+  namespace: dev
+  cert-manager.io/issuer: "letsencrypt-staging"
+spec:
+  - hosts:
+    -  "local-nebari.dev"
+  secretName: local-nebari-dev-tls
+ rules:
+   - host: local-nebari.dev
+     http:
+       paths:
+         - path: /
+           pathType: Prefix
+           backend:
+             service:
+               name: local
+               port:
+                 name: web
+YAML
 }
